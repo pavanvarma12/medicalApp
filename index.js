668564217e324a964e164a282676eb3fa3c0e448
pavan <pavanvarma@ahex.co.in>
2017-06-19 18:22:06 +0530

    validations are added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express=require('express');
 var app= express();
-var bodyParser = require('body-parser');
-//var mongoose =require('mongoose');
+
+var mongoose =require('mongoose');
 var path =require('path');
-//var User= require('./model/usermodel');
+var User= require('./model/usermodel');
+var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 var validator = require('validator');
  // connect to mongodb
@@ -13,87 +14,104 @@ var validator = require('validator');
 //     console.log('connection failed');
 // }
  //middleware service
+mongoose.conect ('mongodb://root:root@ds131742.mlab.com:31742/medicine')
+ 
 app.use(bodyParser.urlencoded({extended:true}));
    app.use(bodyParser.json());
    app.use(expressValidator());
    
    app.get('/', function(req,res){
-    res.send('ok')
+       res.send("ok");
+    //if(req.body.name )
 	
 });
  
-// app.post('/register',function(req,res){
-//     console .log("hii");     
-////     // validate the input
-////  req.checkBody('username', 'Username is required').notEmpty();
-////  req.checkBody('password', 'Password is required').notEmpty();
-////   req.checkBody('email', 'Email is required').notEmpty();
-////  req.checkBody('email', 'Email does not appear to be valid').isEmail();
-////
-////  // check the validation object for errors
-////  var errors = req.validationErrors();
-////
-////  console.log(errors);  
+ app.post('/register',function(req,res){
+     console .log("hii");  
+     
+   //validate the inputs
+  req.checkBody('Adharno', 'adharno is required').notEmpty().isInt();
+  req.checkBody('password', 'Password should be combination of one numerical and one capitalletter').notEmpty().matches(/^(?=.*?[A-Z])(?=.*?[0-9])$/,"i");
+  req.checkBody('repassword', 'Passwords do not match').equals(req.body.password);
+   req.checkBody('email', 'Email is required').notEmpty().isEmail();
+   req.checkBody('Mobile', 'not more than 10').isInt().len(10,10);
+   
+   var errors = req.validationErrors();
+  console.log(errors);  
+            if (errors) {
+                console.log('hi1');
+                // res.render('register', { flash: { type: 'alert-danger', messages: errors }});
+           }
+        else {
+            console.log("hi");
+            res.json({"message":"success"});
+    //res.render('register', { flash: { type: 'alert-success', messages: [ { msg: 'No errors!' }]}});
+          }
+  
+//     var errors = req.validationErrors();
+//               if (errors) {
+//                console.log('hi1');
+//                
+//           }
+//        else {
+//            console.log("hi");
+//            res.send("hi3")
+//    
+//          }
+//  
+//   var errors = req.validationErrors();
 //
-////  if (errors) {
-////    res.render('register', { flash: { type: 'alert-danger', messages: errors }});
-////  }
-////  else {
-////    res.render('register', { flash: { type: 'alert-success', messages: [ { msg: 'No errors!' }]}});
-////  }
-//     
-//   var user = new User({
-//           
-// 	 Adharno:req.body.Adharno,
-// 	 email:req.body.email,
-//         password:req.body.password,
-//         repassword:req.body.confpassword,
-//         Mobile:req.body.Mobile
-// 	});
-////      console.log("hii");
-////     req.checkBody({
-//// 'email': {
-////    optional: {
-////      options: { checkFalsy: true } // or: [{ checkFalsy: true }]
-////    },
-////    isEmail: {
-////      errorMessage: 'Invalid Email'
-////    }
-////  },
-////  'password': {
-////    notEmpty: true,
-////    matches: {
-////      options: ['/^[A-Za-z]\w{0,14}$/', 'i'] // pass options to the validator with the options property as an array
-////      // options: [/example/i] // matches also accepts the full expression in the first parameter
-////    },
-////    errorMessage: 'Invalid Password' // Error message for the parameter
-////  },
-////  'Mobile': { //
-////    optional: true, // won't validate if field is empty
-////    isLength: {
-////      options: [{ min: 2, max: 10 }],
-////      errorMessage: 'Must be between 2 and 10 chars long' // Error message for the validator, takes precedent over parameter message
-////    },
-////    errorMessage: 'not more than 10'
-////    
-////    
-////  }
+//  console.log(errors);  
 //
-//     
-// 	user.save(function(err,user){
-// 		if(err){
-//                    console.log(err);
-// 		res.send(err);	
-// 			
-//        }else{
-//            console.log(user);
-//            //res.json({"message":"useradded successfully"});
-//            res.send(user);
-//                 }
-//        
-// 	});
-//});
-// 
+//            if (errors) {
+//                console.log('hi1');
+//                
+//           }
+//        else {
+//            console.log("hi");
+//            res.send("")
+//    
+//          }
+//  
+//  req.checkBody('Mobile', 'not more than 10').isInt().len(10,10);
+//  
+//  var errors = req.validationErrors();
+//
+//  console.log(errors);  
+//
+//            if (errors) {
+//                
+//           }
+//        else {
+//            console.log('hello');
+//    
+//          }
+     
+   var user = new User({
+           
+ 	 Adharno:req.body.Adharno,
+ 	 email:req.body.email,
+         password:req.body.password,
+         repassword:req.body.repassword,
+         Mobile:req.body.Mobile
+ 	});
+        console.log("helloooo");
+
+     
+ 	user.save(function(err,user){
+ 		if(err){
+                    console.log(err);
+ 		res.send(err);	
+ 			
+        }else{
+            console.log(user);
+            //res.json({"message":"useradded successfully"});
+            res.send(user);
+                 }
+        
+ 	});
+});
+
  
 app.listen(process.env.PORT || 3001,function(){
      console.log('listening the port number is', 3001);
