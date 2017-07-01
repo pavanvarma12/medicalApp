1899c934c3a3b9b39c3a0db188a7269d23e5dcfb
pavan <pavanvarma@ahex.co.in>
2017-07-01 13:44:10 +0530

    port number is added

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ app.set( 'port', ( process.env.PORT || 3003 ));
 
 
   //connect to mongodb
- try{
- mongoose.connect('mongodb://localhost:27017/mean');
- }catch(err){
-     console.log('connection failed');
- }
+// try{
+// mongoose.connect('mongodb://localhost:27017/mean');
+// }catch(err){
+//     console.log('connection failed');
+// }
 
  //middleware service
-//mongoose.connect('mongodb://root:root@ds131742.mlab.com:31742/medicine')
+mongoose.connect('mongodb://root:root@ds131742.mlab.com:31742/medicine')
  
 app.use(bodyParser.urlencoded({extended:true}));
    app.use(bodyParser.json());
