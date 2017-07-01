bdc2b216e9e83f0294cd376cd6bbb9e2e5aa4018
pavan <pavanvarma@ahex.co.in>
2017-07-01 16:32:09 +0530

    some code commented

--- controllers/Register.controller.js
+++ controllers/Register.controller.js
@@ -1,42 +1,42 @@
-//var User= require('../model/usermodel');
-//module.exports.register=function(req,res){
-//        
-//       //validate the inputs
-// req.checkBody('aadharno', 'adharno is integer and required').notEmpty().isInt();
-// req.checkBody('password', 'Password should be combination of one numerical and one capitalletter').notEmpty();
-//  req.checkBody('confirmPassword', 'Passwords do not match').equals(req.body.password);
-//   req.checkBody('email', 'Email is required').notEmpty().isEmail();
-//   req.checkBody('mobile', 'mobile number must be 10 digits').isInt().len(10,10);
-//        
-//   var user = new User({
-//         Adharno:req.body.aadharno,
-// 	 email:req.body.email,
-//         password:req.body.password,
-//         //repassword:req.body.confirmPassword,
-//         Mobile:req.body.mobile
-// 	});
-//        console.log("helloooo");
-//
-//      var errors = req.validationErrors();
-//              if (errors) {
-//                  res.send(errors);
-//                 //return;
-//         //res.render('error', { flash: { type: 'alert-danger', messages: errors }});
-//           }
-//        //else {
-//          
-//    //res.render('register', { flash: { type: 'alert-success', messages: [ { msg: 'No errors!' }]}});
-////}
-// 	user.save(function(err,user){
-// 		if(err){
-//                 
-//           res.status(401).json ({"errmsg":'This  email is already  taken try another'});      	
-//      }else{
-//            
-//            res.status(200).send({"message":"user registered successfully and saved in database"});
-//            //res.send(user);
-//                 }
-//        });
-//  }
+var User= require('../model/usermodel');
+module.exports.register=function(req,res){
+        
+       //validate the inputs
+ req.checkBody('aadharno', 'adharno is integer and required').notEmpty().isInt();
+ req.checkBody('password', 'Password should be combination of one numerical and one capitalletter').notEmpty();
+  req.checkBody('confirmPassword', 'Passwords do not match').equals(req.body.password);
+   req.checkBody('email', 'Email is required').notEmpty().isEmail();
+   req.checkBody('mobile', 'mobile number must be 10 digits').isInt().len(10,10);
+        
+   var user = new User({
+         Adharno:req.body.aadharno,
+ 	 email:req.body.email,
+         password:req.body.password,
+         //repassword:req.body.confirmPassword,
+         Mobile:req.body.mobile
+ 	});
+        console.log("helloooo");
+
+      var errors = req.validationErrors();
+              if (errors) {
+                  res.send(errors);
+                 //return;
+         //res.render('error', { flash: { type: 'alert-danger', messages: errors }});
+           }
+        //else {
+          
+    //res.render('register', { flash: { type: 'alert-success', messages: [ { msg: 'No errors!' }]}});
+//}
+ 	user.save(function(err,user){
+ 		if(err){
+                 
+           res.status(401).json ({"errmsg":'This  email is already  taken try another'});      	
+      }else{
+            
+            res.status(200).send({"message":"user registered successfully and saved in database"});
+            //res.send(user);
+                 }
+        });
+  }
 
 
--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,30 +1,30 @@
-//var bcrypt = require('bcrypt')
-//var User= require('../model/usermodel');
-//var jwt = require('jsonwebtoken');
-//var Secret = 'ahex'; 
-//module.exports.login=function(req,res){
-//  User.findOne({
-//    email: req.body.email
-//  }, function(err, user) {
-//    if (err) throw err;
-//
-//    if (!user) {
-//      res.status(404).send({success: false, msg: 'Authentication failed. User not found.'});
-//    } else if(user) {
-//        if(req.body.password){
-//           var validpassword=user.comparePassword(req.body.password);
-////        }else{
-////            res.json({success:false,message:'no password provided'});
-//        }
-//        if(!validpassword){
-//          res.status(401).json({success:false,message:' Invalid password'});
-//      }else{
-//          var token=jwt.sign(user,Secret,{expiresIn : '1h'});
-//                    
-//          res.status(200).json({success:true,message:'user authenticated',token:token});
-//
-//       }
-//      
-//    }
-//  });
-//  }
+var bcrypt = require('bcrypt-nodejs');
+var User= require('../model/usermodel');
+var jwt = require('jsonwebtoken');
+var Secret = 'ahex'; 
+module.exports.login=function(req,res){
+  User.findOne({
+    email: req.body.email
+  }, function(err, user) {
+    if (err) throw err;
+
+    if (!user) {
+      res.status(404).send({success: false, msg: 'Authentication failed. User not found.'});
+    } else if(user) {
+        if(req.body.password){
+           var validpassword=user.comparePassword(req.body.password);
+//        }else{
+//            res.json({success:false,message:'no password provided'});
+        }
+        if(!validpassword){
+          res.status(401).json({success:false,message:' Invalid password'});
+      }else{
+          var token=jwt.sign(user,Secret,{expiresIn : '1h'});
+                    
+          res.status(200).json({success:true,message:'user authenticated',token:token});
+
+       }
+      
+    }
+  });
+  }
