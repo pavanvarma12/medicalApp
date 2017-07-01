10f3e94622a33ca67dcdd0b47c1e8b1f43b9167f
pavan <pavanvarma@ahex.co.in>
2017-07-01 15:56:42 +0530

    database url added

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 var validator = require('validator');
 var morgan = require('morgan'); 
-var registerController=require('./controllers/Register.controller');
-var loginController=require('./controllers/login.controller');
-var medicineController=require('./controllers/medecine.controller');
+//var registerController=require('./controllers/Register.controller');
+//var loginController=require('./controllers/login.controller');
+//var medicineController=require('./controllers/medecine.controller');
 app.set( 'port', ( process.env.PORT || 3003 ));
 var favicon = require('favicon');
+//var User= require('../model/usermodel');
 
 //app.use(function(req, res, next) {
 //  res.header("Access-Control-Allow-Origin", "*");
@@ -46,11 +47,52 @@ app.set('view engine', 'jade')
        res.send("ok");
     	
 });
+app.post('/register',function(req,res){
+     console .log("hii");  
+     
+   //validate the inputs
+  req.checkBody('aadharno', 'adharno is integer and required').notEmpty().isInt();
+  req.checkBody('password', 'Password should be combination of one numerical and one capitalletter').notEmpty();
+  req.checkBody('confirmPassword', 'Passwords do not match').equals(req.body.password);
+   req.checkBody('email', 'Email is required').notEmpty().isEmail();
+   req.checkBody('mobile', 'mobile number must be 10 digits').isInt().len(10,10);
+   
+   var user = new User({
+         Adharno:req.body.aadharno,
+ 	 email:req.body.email,
+         password:req.body.password,
+         repassword:req.body.confirmPassword,
+         Mobile:req.body.mobile
+ 	});
+      var errors = req.validationErrors();
+              if (errors) {
+                  res.send(errors);
+                 //return;
+         //res.render('error', { flash: { type: 'alert-danger', messages: errors }});
+           }
+        //else {
+          
+    //res.render('register', { flash: { type: 'alert-success', messages: [ { msg: 'No errors!' }]}});
+//}
+ 	user.save(function(err,user){
+ 		if(err){
+                 
+           res.json ({"errmsg":'This  email is already  taken try another'});      	
+      }else{
+            
+            res.status(200).send({"message":"user registered successfully and saved in database"});
+            //res.send(user);
+                 }
+        });
+  });
 
-  app.post('/register',registerController.register);
-  app.post('/login',loginController.login);
-  app.get('/search',medicineController.list);
-  app.post('/medecine',medicineController.medicine);
+
+
+
+//  app.post('/register',registerController.register);
+//  app.post('/login',loginController.login);
+//  app.get('/search',medicineController.list);
+//  app.post('/medecine',medicineController.medicine);
 
 
 //    app.listen(process.env.PORT || 3003,function(){
